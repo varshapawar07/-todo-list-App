b7b8d37cc149f1695e779198a3721a105f984ac2
varsha pawar <varshapawar0704@gmail.com>
2024-12-24 19:13:43 +0530

    Initial commit with main.dart and home.dart

---
+++ android/app/src/main/kotlin/com/example/app1_to_do_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app1_to_do_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromRGBO(255, 151, 196, 1);
+const Color tdBlue = Color.fromARGB(255, 175, 247, 160);
+
+const Color tdBlack = Color.fromARGB(255, 118, 205, 255);
+const Color tdGrey = Color.fromARGB(255, 87, 0, 77);
+
+const Color tdBGColor = Color.fromARGB(195, 125, 61, 5);
